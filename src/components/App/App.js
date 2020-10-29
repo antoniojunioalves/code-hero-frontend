b4154a2521df66c20efccfedee71e8c87d28d4ff
Renan Frutuozo de Meneses <renan.meneses@softplan.com.br>
2020-10-28 22:23:19 -0300

    Primeiro commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.scss'
+
+const App = () => (
+  <div data-testid='app-component' className='App'>
+    <header className='App-header'>
+      <h1>Code Hero</h1>
+    </header>
+  </div>
+)
+
+export default App
---
+++ src/components/App/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+test('renders App component', () => {
+  render(<App />)
+  const component = screen.getByTestId('app-component')
+
+  expect(component).toBeInTheDocument()
+})
